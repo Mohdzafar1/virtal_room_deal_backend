321714f44ebf287af6834652d6d8fa39830c94bd
Mohdzafar1 <mohdzafar97597@gmail.com>
2025-04-02 00:10:28 +0530

    change url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 // Fix CORS issue
 app.use(cors({
-    origin: [process.env.FRONTEND_URL,"http://localhost:3000","http://localhost:3001"], 
+    origin: ["http://localhost:3000","http://localhost:3001","https://virtual-room-deal-frontend.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
